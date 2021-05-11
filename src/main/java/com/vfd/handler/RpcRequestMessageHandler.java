2d54b7c35a7e2cbe2f088050ed0e9ae38b445397
vfdxvffd <2201986113@qq.com>
2021-05-11 17:08:18 +0800

    [更新] 优化远程调用发生异常的时候服务端向客户端通知的信息

--- src/main/java/com/vfd/handler/RpcRequestMessageHandler.java
+++ src/main/java/com/vfd/handler/RpcRequestMessageHandler.java
@@ -18,17 +18,32 @@ import java.lang.reflect.Method;
  */
 @ChannelHandler.Sharable
 public class RpcRequestMessageHandler extends SimpleChannelInboundHandler<RpcRequestMessage> {
+
+    private String interfaceToImplement;
+
+    public RpcRequestMessageHandler() {
+    }
+
+    public RpcRequestMessageHandler(String interfaceToImplement) {
+        this.interfaceToImplement = interfaceToImplement;
+    }
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RpcRequestMessage message) {
         RpcResponseMessage response = new RpcResponseMessage();
         response.setSequenceId(message.getSequenceId());
         try {
-            final Object o = ServicesFactory.getService(message.getInterfaceName());
+            final Object o = ServicesFactory.getService(message.getInterfaceName(), interfaceToImplement);
             final Method method = o.getClass().getMethod(message.getMethodName(), message.getParameterTypes());
             final Object invoke = method.invoke(o, message.getParameterValue());
             response.setReturnValue(invoke);
         } catch (Exception e) {
-            String msg = e.getCause().getMessage();
+            // e.printStackTrace();
+            String msg;
+            final Throwable cause = e.getCause();
+            if (cause == null)  msg = e.getMessage();
+            else    msg= cause.getMessage();
+            if (msg == null)    msg = e.toString();
             final Exception exception = new Exception("Remote procedure call error: " + msg);
             exception.setStackTrace(new StackTraceElement[]{});
             response.setExceptionValue(exception);
